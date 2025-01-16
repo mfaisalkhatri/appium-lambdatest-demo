3984e030d6439cdc3f8d7354a473ee2d4bcf4cac
Mohammad Faisal Khatri <mohammadfaisalkhatri@gmail.com>
2025-01-17 00:07:20 +0300

    added docker compose file and updated project for allure report (#10)
    * added docker-compose for jenkins
    
    * added allure report integration
    
    * updated tests
    
    * formatting changes to BaseTest class
    
    * formatting changes to BaseTest class

--- src/test/java/io/github/mfaisalkhatri/proverbialapp/android/sampletests/tests/ProverbialAppTests.java
+++ src/test/java/io/github/mfaisalkhatri/proverbialapp/android/sampletests/tests/ProverbialAppTests.java
@@ -3,17 +3,32 @@ package io.github.mfaisalkhatri.proverbialapp.android.sampletests.tests;
 import static org.testng.Assert.assertEquals;
 
 import io.github.mfaisalkhatri.proverbialapp.android.sampletests.pages.HomePage;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.testng.Tag;
 import org.testng.annotations.Test;
 
 public class ProverbialAppTests extends BaseTest {
 
+    @Description ("Check that the text on Home page changes on clicking Text Button")
     @Test
+    @Tag ("Android")
     public void testTextOnHomePage () {
+        verifyHomePageTextOnAppLoad ();
+        verifyTextChangeOnTappingTextButton ();
+        this.status = "passed";
+    }
+
+    @Step ("Checking the Home page text when the app loads")
+    public void verifyHomePageTextOnAppLoad () {
         final HomePage homePage = new HomePage (this.androidDriver);
         assertEquals (homePage.getText (), "Hello! Welcome to lambdatest Sample App called Proverbial");
+    }
+
+    @Step ("Checking the text after tapping on Text button")
+    public void verifyTextChangeOnTappingTextButton () {
+        final HomePage homePage = new HomePage (this.androidDriver);
         homePage.tapOnTextBtn ();
         assertEquals (homePage.getText (), "Proverbial");
-        this.status = "passed";
-        System.out.println ("Test Passed!");
     }
 }
